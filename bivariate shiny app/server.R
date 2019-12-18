e8050161e2eb6000c6a7613843eef2e691c31fb9
rbbinder <robin.basalaev-binder@mail.mcgill.ca>
2019-12-18 17:37:18 -0500

    bivariate colors updated (still no map)

--- bivariate shiny app/server.R
+++ bivariate shiny app/server.R
@@ -170,7 +170,6 @@ load(file = "data/data_for_plot.Rdata")
         color = "white",
         size = 0.01
       ) +
-      # scale_fill_identity() +
       scale_fill_identity() +
       theme_map()
 
